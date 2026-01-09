8b18cb8924d807dd76fd551532935dceb8c793e7
DomMzzz <domnuthh@gmail.com>
2026-01-09 20:36:22 +0700

    Add vendor for Vercel

--- api/index.php
+++ api/index.php
@@ -1,11 +1,3 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
-
-$request = Illuminate\Http\Request::capture();
-$response = $app->handle($request);
-
-$response->send();
-$app->terminate($request, $response);
+require __DIR__ . '/../public/index.php';
